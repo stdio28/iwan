fb14d85ba00bc08e60c37139065709218207b17f
rahmatsetiawan <rahmatsetiawan1994@gmail.com>
2015-06-01 16:42:02 +0700

    versi 2.3.3

---
+++ index.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+
+</head>
+<body>
+<form action="st.php" method="post">
+
+<h1> Tokoh St-Wan</H1><br>
+
+
+
+<form>
+
+
+Silahkan Pilih Barang yang Ingin dibeli :
+<p>
+
+<input type="checkbox" name="TV" value="TV">1. TV <br>
+<input type="checkbox" name="Meja_Belajar" value="Meja Belajar">2. Meja Belajar <br>
+<input type="checkbox" name="Kursi" value="Kursi">3. Kursi <br>
+<input type="checkbox" name="Lemari" value="Lemari">4. Lemari 
+<p>
+
+<input type="checkbox" name="Buku" value="Buku">5. Buku <br>
+<input type="checkbox" name="Pensil" value="Pensil">6. Pensil <br>
+<input type="checkbox" name="Ember" value="Ember">7. Ember <br>
+<input type="checkbox" name="Sapu" value="Sapu">8. Sapu
+<p>
+
+<table width="152" border="0">
+  <tr>
+    <td width="64"><input type="submit" value="Beli"></td>
+    <td width="78"><input type="reset" value="Reset"></td>
+  </tr>
+</table>
+<p>
+
+</form>
+
+
+</body>
+</html>
---
+++ st.php
@@ -0,0 +1,73 @@
+<?php
+
+
+if (isset($_POST['TV']))
+
+    {
+    echo "".$_POST['TV']."<br>";
+    echo "Harga        : Rp " .$harga[0]=300000;
+    echo "<br><p>";
+}
+
+if (isset($_POST['Meja_Belajar']))
+ {  
+    echo "".$_POST['Meja_Belajar']."<br>";
+    echo "Harga        : Rp " .$harga[1]=50000;
+    echo "<br><p>";
+}
+if (isset($_POST['Kursi']))
+ {  
+    echo "".$_POST['Kursi']."<br>";
+    echo "Harga        : Rp " .$harga[2]=115000;
+    echo "<br><p>";;
+}
+if (isset($_POST['Lemari']))
+ {  
+    echo "".$_POST['Lemari']."<br>";
+    echo "Harga        : Rp " .$harga[3]=450000;
+    echo "<br><p>";
+}
+
+
+if (isset($_POST['Buku']))
+
+    {
+    echo "".$_POST['Buku']."<br>";
+    echo "Harga        : Rp " .$harga[0]=125000;
+    echo "<br><p>";
+}
+
+if (isset($_POST['Pensil']))
+ {  
+    echo "".$_POST['Pensil']."<br>";
+    echo "Harga        : Rp " .$harga[1]=5000;
+    echo "<br><p>";
+}
+if (isset($_POST['Ember']))
+ {  
+    echo "".$_POST['Ember']."<br>";
+    echo "Harga        : Rp " .$harga[2]=10000;
+    echo "<br><p>";
+}
+if (isset($_POST['Sapu']))
+ {  
+    echo "".$_POST['Sapu']."<br>";
+    echo "Harga        : Rp " .$harga[3]=70000;
+     echo "<br><p>";
+}
+
+$jum_bayar=array_sum($harga); 
+echo "Total Bayar Anda Sebesar = Rp $jum_bayar <br>";
+
+?>
+
+<p>
+<form id="form1" name="form1" method="" action="index.php">
+  <label>
+  <input type="submit" name="Back" id="Back" value="Kembali" />
+  </label>
+</form>
+<p>
+
+</body>
+</html>
